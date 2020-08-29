2b165aea19299ec67edb9f0a439d325ff99dfbc0
Joshua Browne <joshua.browne@s.elim.school.nz>
2020-08-29 20:42:50 +1200

    Transaction API setup
    - software now recognises HTTP requests to add transactions as well as
    get requests to recieve the current transaction pool
    - each instance of the network now publishes changes to its transaction
    pool to other nodes on the network which then add them to their own
    pools

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
-const pubsub = new PubSub({ blockchain });
+const pubsub = new PubSub({ blockchain, transactionPool, wallet });
 
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
@@ -49,6 +49,8 @@ app.post('/api/transact', (req, res) => {
 
     transactionPool.setTransaction(transaction);
 
+    pubsub.broadcastTransaction(transaction);
+
     res.json({ type: 'success', transaction });
 });
 
@@ -56,7 +58,7 @@ app.get('/api/transaction-pool-map', (req, res) => {
     res.json(transactionPool.transactionMap);
 });
 
-const syncChains = () => {
+const syncWithRootState = () => {
     request({url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response, body) => {
         if(!error && response.statusCode === 200) {
             const rootChain = JSON.parse(body);
@@ -65,6 +67,14 @@ const syncChains = () => {
             blockchain.replaceChain(rootChain);
         }
     })
+    request({url: `${ROOT_NODE_ADDRESS}/api/transaction-pool-map`}, (error, response, body) => {
+        if(!error && response.statusCode === 200) {
+            const rootTransactionMap = JSON.parse(body);
+            
+            console.log('replace transactionMap on a sync with', rootTransactionMap);
+            transactionPool.setMap(rootTransactionMap);
+        }
+    });
 }
 
 let PEER_PORT;
@@ -80,6 +90,6 @@ app.listen(PORT, () => {
 
     if(PORT !== DEFAULT_PORT)
     {
-    syncChains();
+        syncWithRootState();
     }
 });
