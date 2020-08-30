1d9cce36df46ff51ff342ca47a6365124019ac54
Joshua Browne <joshua.browne@s.elim.school.nz>
2020-08-30 18:44:56 +1200

    Web Client
    - now hosted at localhost:3000 is a react web server, which displays the
    current block chain with primitive show more button to show data held
    within a block

---
+++ client/src/components/Block.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+
+class Block extends Component {
+    state = {displayTransaction: false};
+
+    toggleTransaction = () => {
+        this.setState({ displayTransaction: !this.state.displayTransaction });
+    }
+
+    get displayTransaction() {
+        const { data } = this.props.block;
+
+        const stringifiedData = JSON.stringify(data);
+
+        const dataDisplay = stringifiedData.length > 35 ? 
+            `${stringifiedData.substring(0,35)}...`:
+            stringifiedData;
+
+        if (this.state.displayTransaction) {
+            return (
+                <div> 
+                    {JSON.stringify(data)}
+                    <br />
+                    <Button
+                        bsStyle="danger"
+                        bsSize="sm"
+                        onClick={this.toggleTransaction}
+                    >
+                        <b>Show Less</b>
+                    </Button>
+
+                </div>
+            );
+        }
+
+        return (
+        <div>
+            <div><b>Data:</b> {dataDisplay}</div> 
+            <Button
+                bsStyle="danger"
+                bsSize="sm"
+                onClick={this.toggleTransaction}
+            >
+                <b>Show More</b>
+            </Button>
+        </div>
+        );
+    }
+
+
+    render() {
+        const { timestamp, hash } = this.props.block;
+
+        const hashDisplay = `${hash.substring(0,15)}...`;
+        
+
+        return ( 
+            <div className='Block'>
+                <div><b>Hash:</b> {hashDisplay}</div>
+                <div><b>Timestamp:</b> {new Date(timestamp).toLocaleString()}</div>
+                <div>{this.displayTransaction}</div>
+            </div>
+        )
+        
+    }
+}
+
+export default Block;
